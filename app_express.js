fdbc54b1d1c2c7dc652dc818cceb5b40bffab848
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2022-11-24 17:27:29 +0100

    Change paths to form and form result

--- app_express.js
+++ app_express.js
@@ -7,46 +7,57 @@ app.use(express.urlencoded({ extended: true }));
 const pug = require("pug");
 app.set('view engine', 'pug');
 
-// Share the static files from "./static/" folder
+// Publish the static files from './static/' folder under '/' path
 app.use(express.static('static'));
 
-// Share the static files from "/"
+// Publish the static files from './images/' folder under '/img' path
 app.use('/img', express.static('images'));
 
-// Compare app.js: you don't have to set status code or content type
+// Compare res.send() to res.end() in app.js: 
+// You don't have to set status code or content type
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Use simple Pug template to display web page. Notify use of arguments
-// Try to change true to false and refresh the web page
-app.get('/index', (req, res) => {
-  res.render('index', { title: 'Pug template', youAreUsingPug: true});
+// Use simple Pug template to display web page: http://127.0.0.1:5000/pug
+// Notify:
+// - use of template arguments: try to change 'false' to 'true' and refresh the web page
+// - use of 'res.render()' instead of 'res.send()'
+app.get('/pug', (req, res) => {
+  res.render('index', { pageTitle: 'Pug template', youAreUsingPug: false});
 })
 
+// Passing arguments in request path.
+// Try to open http://127.0.0.1:5000/pug/Ann and watch page title
+// Replace 'Ann' with your name
+app.get('/pug/:user', (req, res) => {
+  titleText = `${req.params.user} uses Pug`;
+  res.render('index', { pageTitle: titleText, youAreUsingPug: true});
+})
 
 // Use Pug template to display list (as a list and as a table)
+// Watch how template languages.pug' iterates over the list (each ... in ...)
 app.get('/languages', (req, res) => {
   list = ["Java","Python","JavaScript"];
   res.render('languages', {languages: list});
 })
 
-// Use form to send data
-// Form is displayed after GET request
-app.get('/feedback', function (req, res) {
+// Use the form to send data
+// Form is displayed using Pug template: http://127.0.0.1/feedbackform
+app.get('/feedbackform', function (req, res) {
     res.render('feedback_form');
   });
 
 // Display the content of the form
 // POST request is issued when you click "Submit" button in the form
-app.post("/feedback", function (req, res) {
+app.post("/feedbacksummary", function (req, res) {
     username=req.body.name;
     feedback=req.body.feedback;
     res.render('feedback_summary',
         { username: username, feedback: feedback });
   });
 
-// Start app
+// Start the app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
