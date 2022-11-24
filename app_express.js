8c1bba5902c69696940aa181c88a70c02524cfd0
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2022-11-24 16:11:36 +0100

    app_express.js: add comments

--- app_express.js
+++ app_express.js
@@ -7,22 +7,38 @@ app.use(express.urlencoded({ extended: true }));
 const pug = require("pug");
 app.set('view engine', 'pug');
 
-app.use('/static', express.static('static'));
+// Share the static files from "./static/" folder
+app.use(express.static('static'));
 
+// Share the static files from "/"
+app.use('/img', express.static('images'));
+
+// Compare app.js: you don't have to set status code or content type
 app.get('/', (req, res) => {
-  res.send('Hello Worlds!');
+  res.send('Hello World!');
 });
 
+// Use simple Pug template to display web page. Notify use of arguments
+// Try to change true to false and refresh the web page
+app.get('/index', (req, res) => {
+  res.render('index', { title: 'Pug template', youAreUsingPug: true});
+})
+
+
+// Use Pug template to display list (as a list and as a table)
 app.get('/languages', (req, res) => {
   list = ["Java","Python","JavaScript"];
   res.render('languages', {languages: list});
 })
 
-// Handling get request
+// Use form to send data
+// Form is displayed after GET request
 app.get('/feedback', function (req, res) {
     res.render('feedback_form');
   });
 
+// Display the content of the form
+// POST request is issued when you click "Submit" button in the form
 app.post("/feedback", function (req, res) {
     username=req.body.name;
     feedback=req.body.feedback;
@@ -30,7 +46,7 @@ app.post("/feedback", function (req, res) {
         { username: username, feedback: feedback });
   });
 
-
+// Start app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
